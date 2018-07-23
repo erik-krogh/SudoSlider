a577f715571c91a8e838b993627747e6b0650160
Erik Krogh Kristensen <erik@webbies.dk>
2018-07-23 21:04:19 +0200

    3.5.0

--- js/jquery.sudoSlider.js
+++ js/jquery.sudoSlider.js
@@ -1,5 +1,5 @@
 /**@preserve
- * Sudo Slider version 3.4.10 - jQuery plugin
+ * Sudo Slider version 3.5.0 - jQuery plugin
  * Written by Erik Krogh Kristensen erik@webbies.dk.
  * http://webbies.dk/SudoSlider/
  *
